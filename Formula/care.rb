961c728f9a735f2a795728400c1dc69ddadeeae4
Johan Kool <johankool@users.noreply.github.com>
2024-02-05 20:03:44 +0100

    Publish care 0.0.1

---
+++ Formula/care.rb
@@ -0,0 +1,30 @@
+class Care < Formula
+  desc "Configure apps remotely"
+  homepage "https://github.com/egeniq/app-remote-config"
+  url "https://github.com/egeniq/app-remote-config.git",
+    tag:      "0.0.1",
+    revision: "8cfe0c1a15d54831ead482fd9fe0b60b5f17aa8e"
+  version "0.0.1"
+  license "MIT"
+  version_scheme 1
+  head "https://github.com/egeniq/app-remote-config.git", branch: "main"
+
+  depends_on xcode: ["15.2", :build]
+  uses_from_macos "swift"
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release", "--product", "care"
+    bin.install ".build/release/care"
+  end
+
+  test do
+    shell_output("#{bin}/care init test.yaml")
+    expected_output = 
+"""This configuration is \e[0;32mvalid\e[0;0m.
+\e[0;32m[INFO]\e[0;0m This configuration is in YAML. This is not suitable for publication. Use the prepare command to convert to JSON. - \e[0;2m/\e[0;0m
+\e[0;36m[HINT]\e[0;0m Use the resolve command to verify the output is as expected for an app.
+\e[0;36m[HINT]\e[0;0m Use the prepare command to prepare the configuration for publication.
+"""
+    assert_equal(expected_output, shell_output("#{bin}/care verify test.yaml"))
+  end
+end
