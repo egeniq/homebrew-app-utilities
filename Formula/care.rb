2ecc62ebf5129550044bb07be144be753ade4499
Johan Kool <johankool@users.noreply.github.com>
2025-06-12 08:26:14 +0200

    Bump care to 0.6.1

--- Formula/care.rb
+++ Formula/care.rb
@@ -1,12 +1,12 @@
 class Care < Formula
   desc "Configure apps remotely"
-  homepage "https://github.com/egeniq/app-remote-config-care"
-  url "https://github.com/egeniq/app-remote-config-care.git",
-    tag:      "0.6.0",
-    revision: "8e0c20702c48cea57ec15ba4d96ff3391219af10"
+  homepage "https://github.com/egeniq/app-remote-config"
+  url "https://github.com/egeniq/app-remote-config.git",
+    tag:      "0.6.1",
+    revision: "0a8e68c01704c53412f3394d468dbb0b38304a56"
   license "MIT"
   version_scheme 1
-  head "https://github.com/egeniq/app-remote-config-care.git", branch: "main"
+  head "https://github.com/egeniq/app-remote-config.git", branch: "main"
 
   depends_on xcode: [">= 15.4", :build]
   uses_from_macos "swift"
